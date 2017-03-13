a3a4e1351ecfd18eb77e2cc394ffca0532714608
Elliot Kauffmann <elliot.kauffmann@etu.unice.fr>
2017-03-14 00:11:02 +0100

    more fxml cleanup. (last commit I swear)

--- enseigne/src/main/java/fr/polytech/si3/ihm/controller/AboutController.java
+++ enseigne/src/main/java/fr/polytech/si3/ihm/controller/AboutController.java
@@ -11,5 +11,7 @@ public class AboutController extends Controller {
     public void initialize() {
         displayHeader(this);
         displayContact();
+        String description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur neque orci, pharetra quis porttitor sit amet, maximus eget arcu. Phasellus non tortor vel mauris cursus convallis sed at nulla. Praesent venenatis semper volutpat. Phasellus et erat eu ipsum convallis volutpat. Fusce non purus in enim pharetra molestie. Curabitur fermentum venenatis dolor congue aliquam. Suspendisse ut vulputate dolor. Sed tincidunt gravida nunc eget bibendum. Praesent elementum mi vitae urna elementum commodo. Nullam tempus lacus sed fermentum placerat.";
+        enseigneDescription.setText(description);
     }
 }
--- enseigne/src/main/java/fr/polytech/si3/ihm/model/ListeInfos.java
+++ enseigne/src/main/java/fr/polytech/si3/ihm/model/ListeInfos.java
@@ -12,10 +12,10 @@ public class ListeInfos {
     public ListeInfos() {
 
         listeInfos = FXCollections.observableArrayList(new ArrayList<Info>());
-        listeInfos.add(new Info("Information 1", "Nouvelle gamme de\nproduits disponibles en mai !", "FFFFFF", "/images/information.png"));
-        listeInfos.add(new Info("Information 2", "Fermeture exceptionnelle\ndu S.A.V. en avril", "DDDDDD", "/images/tboth.png"));
-        listeInfos.add(new Info("Information 3", "Nouvel arrivage de CD\nen juin", "FFFFFF", "/images/p_C.jpg"));
-        listeInfos.add(new Info("Information 4", "J'avais plus d'idées", "DDDDDD", "/images/information.png"));
+        listeInfos.add(new Info("Information 1", "Nouvelle gamme de produits disponibles en mai !", "FFFFFF", "/images/information.png"));
+        listeInfos.add(new Info("Information 2", "Fermeture exceptionnelle du S.A.V. en avril", "DDDDDD", "/images/tboth.png"));
+        listeInfos.add(new Info("Information 3", "Nouvel arrivage de CD en juin", "FFFFFF", "/images/p_C.jpg"));
+        listeInfos.add(new Info("Information 4", "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum ", "DDDDDD", "/images/information.png"));
     }
 
     public ObservableList<Info> getListeInfos() {
