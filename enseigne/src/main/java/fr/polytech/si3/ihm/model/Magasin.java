581a05b7739caac00541c64b32f748bb5f109ebd
Elliot Kauffmann <elliot.kauffmann@etu.unice.fr>
2017-03-14 21:34:07 +0100

    final css tweaks

--- enseigne/src/main/java/fr/polytech/si3/ihm/model/Magasin.java
+++ enseigne/src/main/java/fr/polytech/si3/ihm/model/Magasin.java
@@ -2,16 +2,19 @@ package fr.polytech.si3.ihm.model;
 
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.beans.value.ObservableValue;
 
 public class Magasin {
     private StringProperty name;
     private StringProperty city;
     private StringProperty region;
+    private StringProperty site;
 
-    public Magasin(String name, String city, String region){
+    public Magasin(String name, String city, String region, String site){
         this.name= new SimpleStringProperty(name);
         this.city= new SimpleStringProperty(city);
         this.region= new SimpleStringProperty(region);
+        this.site= new SimpleStringProperty(site);
     }
 
     public String getName(){
@@ -24,5 +27,21 @@ public class Magasin {
 
     public String getRegion(){
         return this.region.get();
+    }
+
+    public ObservableValue<String> getNameProperty() {
+        return name;
+    }
+
+    public ObservableValue<String> getCityProperty() {
+        return city;
+    }
+
+    public ObservableValue<String> getRegionProperty() {
+        return region;
+    }
+
+    public ObservableValue<String> getSiteProperty() {
+        return site;
     }
 }
